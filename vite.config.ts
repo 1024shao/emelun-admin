43f4de0f1aaea48d3fa47254e57997252df9b6f2
shaoxueping <shaoxueping@bytedance.com>
2022-08-19 00:36:39 +0800

    feat: add pre-push husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import iconify from '@iconify-json/ic'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import iconify from "@iconify-json/ic";
 import Unocss from "./config/unocss";
 const rollupOptions = {
   external: ["vue", "vue-router"],
@@ -11,29 +12,31 @@ const rollupOptions = {
     },
   },
 };
+
 export default defineConfig({
-  plugins:[vue(),vueJsx({
-}), Unocss()
-],
+  plugins: [vue(), vueJsx({}), Unocss()],
   build: {
     rollupOptions,
-    minify:false,
+    minify: "terser", // boolean | 'terser' | 'esbuild'
+    sourcemap: true, // 输出单独 source文件
+    brotliSize: true, // 生成压缩大小报告
+    cssCodeSplit: true,
     lib: {
       entry: "./src/entry.ts",
       name: "EmulunUI",
       fileName: "emulun-ui",
       // 导出模块格式
-      formats: ["es", "umd","iife"],
+      formats: ["esm", "umd", "iife"],
     },
   },
   test: {
     globals: true,
     // simulate DOM with happy-dom
     // (requires installing happy-dom as a peer dependency)
-    environment: 'happy-dom',
+    environment: "happy-dom",
     // 支持tsx组件，很关键
     transformMode: {
-      web: [/.[tj]sx$/]
-    }
-  }
-})
+      web: [/.[tj]sx$/],
+    },
+  },
+});
